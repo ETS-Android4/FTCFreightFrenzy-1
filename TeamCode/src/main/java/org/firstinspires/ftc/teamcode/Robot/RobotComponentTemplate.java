8c34335a417b018897aaac797b9fea39e17652c3
TBchip <bischoffthijs@gmail.com>
2021-06-07 21:02:35 +0200

    added robotComponentTemplate

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/RobotComponentTemplate.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.Robot;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class RobotComponentTemplate extends RobotComponent{
+
+    public RobotComponentTemplate(HardwareMap hardwareMap, MainRobot inputRobot) {
+        super(inputRobot);
+
+        //initializing method
+    }
+
+    @Override
+    public void startThreads(){
+        //start threads here if necessary
+    }
+}
