082b42316d3b2a6afce3e58e1c5335c1457d5abd
TBchip <bischoffthijs@gmail.com>
2021-10-13 20:56:09 +0200

    fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/SampleMecanumDrive.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.RoadRunner.drive;
 
 import androidx.annotation.NonNull;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.control.PIDCoefficients;
 import com.acmerobotics.roadrunner.drive.DriveSignal;
 import com.acmerobotics.roadrunner.drive.MecanumDrive;
