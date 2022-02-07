897433b8056913fb88b33db9e689e70859903113
EmoeBos <58362791+EmoeBos@users.noreply.github.com>
2022-02-07 18:04:55 +0100

    warehuis

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TeamRed_Towarehous.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.RoadRunner.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.Robot.MainRobot;
+
+@Autonomous(name="TeamRed_Towarehous", group="")
+public class TeamRed_Towarehous extends LinearOpMode {
+    private MainRobot robot;
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        String[] enabledComponents = {"duckArm"};
+        robot = new MainRobot(hardwareMap, telemetry, enabledComponents, this);
+
+        robot.logging.setLog("state", "Initializing");
+        robot.startThreads();
+        robot.initDrive(DcMotor.RunMode.RUN_USING_ENCODER, DcMotor.ZeroPowerBehavior.FLOAT, new Pose2d(-31 , -62, Math.toRadians(180)));
+        robot.logging.setLog("state", "Initialized, waiting for start");
+
+        waitForStart();
+
+        robot.logging.setLog("state", "Running");
+        autonomousSequence();
+
+        robot.stopRobot();
+        robot.logging.setLog("state", "Stopped");
+    }
+
+    //autonomous sequence
+    private void autonomousSequence() throws InterruptedException {
+        //this is the sequense for moving to the warehuis
+        TrajectorySequence trajectory = robot.drive.trajectorySequenceBuilder(new Pose2d(-31, -69, Math.toRadians(180)))
+                .splineTo(new Vector2d(10, 10), Math.toRadians(90))
+                .build();
+        robot.drive.followTrajectorySequence(trajectory);
+    }
+}
